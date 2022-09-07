22a213cca57df939d66ea3f0aec77234fb706869
Daniel Huffman <drhuffman12@gmail.com>
2022-09-06 23:34:03 -0400

    formatting

--- src/md_array.cr
+++ src/md_array.cr
@@ -1,22 +1,22 @@
 class MdArray
   VERSION = {{ `shards version "#{__DIR__}"`.chomp.stringify }}
 
-  class OrdinateError < Exception ; end
+  class OrdinateError < Exception; end
 
-  getter qty_cells : Int32 # = 0_u64
-  getter dims : Array(Int32) # = [1,1]
+  getter qty_cells : Int32          # = 0_u64
+  getter dims : Array(Int32)        # = [1,1]
   getter dim_factors : Array(Int32) # = [1,1]
-  getter seeds : Array(Float64) # = 0_f64
-  property cells # : StaticArray
+  getter seeds : Array(Float64)     # = 0_f64
+  property cells                    # : StaticArray
 
-  def initialize(@dims : Array(Int32) = [1,1], @seeds = [0.0])
+  def initialize(@dims : Array(Int32) = [1, 1], @seeds = [0.0])
     @qty_cells = dims.product(1).to_i32
     @dim_factors = [1] # fake value for initial seed for compiler sake
     @dim_factors = recalc_dim_factors
     @cells = Array(Float64).new(@qty_cells) { |i| @seeds[i % @seeds.size] }
   end
 
-  def initialize(@dims : Array(Int32) = [1,1], &seeds_block)
+  def initialize(@dims : Array(Int32) = [1, 1], &seeds_block)
     @qty_cells = dims.product(1).to_i32
     @dim_factors = [1] # fake value for initial seed for compiler sake
     @dim_factors = recalc_dim_factors
@@ -35,7 +35,7 @@ class MdArray
 
   def recalc_dim_factors
     j = 1
-    x = @dims[0..-2].map_with_index{|b,i| k = j*b; j = k; k }
+    x = @dims[0..-2].map_with_index { |b, i| k = j*b; j = k; k }
     @dim_factors = [1] + x
   end
 
@@ -44,7 +44,7 @@ class MdArray
     dims.map_with_index do |dim, i|
       v = ordinates[i]
       if i >= 0
-        v = @dim_factors[i-1] * v
+        v = @dim_factors[i - 1] * v
       end
       v
     end.sum
